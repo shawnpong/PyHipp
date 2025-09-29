5942aacdbe64276a463cde08a6af6cdaf02a9949
AWS ParallelCluster user <ec2-user@ip-10-0-6-134.ap-southeast-1.compute.internal>
2025-09-29 04:22:52 +0000

    Added waveform files for quiz 1B

---
+++ waveformall-slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "waveformall-slurm"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o waveformall-slurm.%N.%j.out # STDOUT
+#SBATCH -e waveformall-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
+import DataProcessingTools as DPT; \
+wfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.Waveform); \
+wfall.save();"
+
